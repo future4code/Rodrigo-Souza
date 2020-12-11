6c6a6b91d9590b34abd6d4463bd3bf216bdf93bd
rsouza95 <rsouzawork95@gmail.com>
2020-12-11 14:27:24 -0300

    segundo ex de interpretação de código feito

---
+++ semana-4/lista-de-exercicios/interpretacao-de-codigo/2/index.js
@@ -0,0 +1,7 @@
+// cria uma função chamada investeDinheiro, como parâmetros (tipoDeInvestimento, valor). Dentro dessa função encontramos uma variável valorAposInvetismento e dentro dela uma estrutura
+// de switch case que é de repetição (tipoDeInvestimento) usando o case Poupança, Renda Fixa, CDB, Ações e o default(else). Usando como operação o valor do parâmetro * um número qualquer já chumbado.
+// Uma mensagem de alert irá aparecer caso o tipo de investimento for informado incorretamente. Pra finalizar a função, é utilizado o return ValorAposInvestimento. Posteriormente são criadas 2 variáveis, novoMontante e segundoMontante,
+// o primeiro vai pegar o case de Ações e substituir o valor pelo número 150, e o segundo vai fazer o mesmo  mas vai ser substuido por 200.
+// o primeiro console.log vai resultar no valor 165 -> 150 *1,1
+// e o segundo console.log náo vai imprimir nada, só mostrar no console undefined, pois em nenhum momento "Tesouro Direto" foi definido no case.
+
