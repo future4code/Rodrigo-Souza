4acfc69999694e59a0624e60bd1ad5d1ddcd9b1b
rsouza95 <rsouzawork95@gmail.com>
2020-12-11 15:07:00 -0300

    segundo ex lógica de programação feito

---
+++ semana-4/lista-de-exercicios/logica-de-programacao/2/index.js
@@ -0,0 +1,22 @@
+//2. Para este exercício considere as seguintes variáveis:
+
+    const booleano1 = true
+    const booleano2 = false
+    const booleano3 = !booleano2 // true
+    const booleano4 = !booleano3 // false
+
+
+//a) booleano1 && booleano2 && !booleano4
+// false
+
+//b) (booleano1 && booleano2) || !booleano3
+//false
+
+//c) (booleano2 || booleano3) && (booleano4 || booleano1)
+//true
+
+//d) !(booleano2 && booleano3) || !(booleano1 && booleano3)
+//false
+
+//e)!(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)
+//true
